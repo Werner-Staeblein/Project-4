58338e387075035d93d99c0afc6e2ed14c0dc787
Werner <wstablein@hotmail.com>
2024-08-06 14:07:26 +0200

    Delete test app. Create django blog app. Register new blog name. Test. Test passed

--- diviblog/urls.py
+++ diviblog/urls.py
@@ -4,9 +4,11 @@ URL configuration for testproject project.
 """
 from django.contrib import admin
 from django.urls import path
-from hello_world import views as index_views
+from blog import views as index_views
 
 urlpatterns = [
-    path('hello/', index_views.index, name="index"),
+    path('blog/', index_views.index, name="index"),
     path('admin/', admin.site.urls),
 ]
+
+
