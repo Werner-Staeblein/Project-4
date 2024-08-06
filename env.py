b1cf531c7be5d661f8b906429a2a736f23302b7b
Werner <wstablein@hotmail.com>
2024-08-06 09:20:53 +0200

    Stop tracking of env.py

--- env.py
+++
@@ -1,4 +0,0 @@
-import os
-
-os.environ['DATABASE_URL'] = 'postgres://ux8cdzuvabm:Fz8MrfiraErR@ep-gentle-mountain-a23bxz6h-pooler.eu-central-1.aws.neon.tech/slept_finch_silly_529587'
-os.environ['SECRET_KEY'] = 'S"N|BqlG+|+EVC/Y2=wVXXzQL+0fh<'
