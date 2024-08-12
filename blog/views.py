070b150d1877389153d1b6ea68286a1b11dc343b
Werner <wstablein@hotmail.com>
2024-08-12 10:12:34 +0200

    Change views.py to have context variable with data passed on to index.html

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import DividendPosts
 
-# Create your views here.
+# When path is /blog then index is called / database with blog entries(objects) is queried and stored in post_list
+# database queried data stored in post_list passed on as context to index.html
+# I query for published "objects" or blog entries in database that are status of 1 only
 
 def index(request):
-    return HttpResponse("Welcome to the Blog index page!")
+    post_list = DividendPosts.objects.filter(status=1)  
+    context = {
+        'post_list': post_list,
+    }
+    return render(request, 'index.html', context)
