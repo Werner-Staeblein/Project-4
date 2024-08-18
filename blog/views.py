d63e3e9606076b739fb0a2bab583242fd3e293cc
Werner <wstablein@hotmail.com>
2024-08-18 20:44:24 +0200

    Add user status messages on login/logout status

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
 from .models import DividendPosts
 
 def landing(request):
