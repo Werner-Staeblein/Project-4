a655831c485a4c1e1b09ec5ff21dc5fe46167953
Werner <wstablein@hotmail.com>
2024-08-18 15:51:08 +0200

    Wire up allauth in settings.py of dividblog django project

--- diviblog/settings.py
+++ diviblog/settings.py
@@ -43,9 +43,19 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
     'django_summernote',
     'blog',
 ]
+
+# The constants for allauth / redirection to home page when logged in or logged out
+
+SITE_ID = 1
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -56,6 +66,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'allauth.account.middleware.AccountMiddleware',
 ]
 
 ROOT_URLCONF = 'diviblog.urls'
@@ -96,6 +107,9 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+# This is AUTH_PASSWORD_VALIDATOR / with 'none' Django does not expect email verification in allauth
+
+ACCOUNT_EMAIL_VERIFICATION = 'none'
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
