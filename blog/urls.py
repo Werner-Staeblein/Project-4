e4f4ed0798424a4c2c602a9b85a75ddc9196ee58
Werner <wstablein@hotmail.com>
2024-08-19 10:55:22 +0200

    Adjust CSS custom styling of AllAuth messages

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='blog_index'),
-    path('logout/', views.custom_logout_view, name='account_logout'),
     path('<slug:post_slug>/', views.single_post, name='single_post'),
 ]
