3f083fc75ffb97ba544e91c14031d9af80cba741
Werner <wstablein@hotmail.com>
2024-08-28 13:31:10 +0200

    Redo all of delete functionality incl js and modal. Fix issue that close button in mmodel does not work. Test. Test passed

--- static/js/script.js
+++ static/js/script.js
@@ -1,24 +1,25 @@
 document.addEventListener('DOMContentLoaded', function () {
-    // Collect all the delete buttons and store in constant
 
-    const deleteButtons = document.querySelectorAll('.delete-button');
+    
+    function setDeleteUrl(commentUrl) {
+        const deleteConfirmButton = document.getElementById('deleteConfirm');
+        deleteConfirmButton.setAttribute('href', commentUrl);
+    }
 
-    deleteButtons.forEach(function (button) {
-        button.addEventListener('click', function (event) {
-
-            // Step 1: Prevent default action of the link = NO immediate naviagte to delete URL
-            event.preventDefault();
-
-            const commentId = button.getAttribute('data-comment-id');
-            const deleteUrl = "{% url 'delete_comment' post.slug 0 %}".replace("0", commentId);
-
-            // Step 2: Set the href attribute of the delete button in the modal
-            const deleteConfirmButton = document.getElementById('deleteConfirm');
-            deleteConfirmButton.setAttribute('href', deleteUrl);
-
-            // Step 3: Make the modal visible
-            const deleteModal = new bootstrap.Modal(document.getElementById('ModalForDelete'));
-            deleteModal.show();
+    // 
+    document.querySelectorAll('.delete-button').forEach(button => {
+        button.addEventListener('click', function() {
+    
+            // 
+            const commentUrl = this.getAttribute('data-comment-url');
+    
+            // 
+            setDeleteUrl(commentUrl);
+    
+            // 
+            const modal = new bootstrap.Modal(document.getElementById('ModalForDelete'));
+            modal.show();
         });
     });
+
 });
