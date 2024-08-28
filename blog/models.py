9d31b144ec77465b34bf5c0fa08ca6511221c531
Werner <wstablein@hotmail.com>
2024-08-28 16:34:13 +0200

    Run CI pep8 linter on .py files. All .py files no clear with no errors found

--- blog/models.py
+++ blog/models.py
@@ -8,7 +8,11 @@ STATUS_CHOICES = ((0, "Draft"), (1, "Published"))
 class DividendPosts(models.Model):
     headline = models.CharField(max_length=255, unique=True)
     slug = models.SlugField(max_length=255, unique=True)
-    writer = models.ForeignKey(User, on_delete=models.CASCADE, related_name="articles")
+    writer = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="articles"
+    )
     body = models.TextField()
     published_date = models.DateTimeField(auto_now_add=True)
     last_modified = models.DateTimeField(auto_now=True)
@@ -22,8 +26,16 @@ class DividendPosts(models.Model):
 
 
 class Discussion(models.Model):
-    article = models.ForeignKey(DividendPosts, related_name="discussions", on_delete=models.CASCADE)
-    commentator = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_discussions")
+    article = models.ForeignKey(
+        DividendPosts,
+        related_name="discussions",
+        on_delete=models.CASCADE
+    )
+    commentator = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="user_discussions"
+    )
     comment_text = models.TextField()
     approved = models.BooleanField(default=False)
     comment_date = models.DateTimeField(auto_now_add=True)
--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,8 @@ from .views import blog_index, blogpost_detail, comment_edit, delete_comment
 urlpatterns = [
     path('', blog_index, name='blog_index'),
     path('post/<slug:slug>/', blogpost_detail, name='blogpost_detail'),
-    path('post/<slug:slug>/edit_comment/<int:comment_id>/', comment_edit, name='comment_edit'),
-    path('post/<slug:slug>/delete_comment/<int:comment_id>/', delete_comment, name='delete_comment'),
+    path('post/<slug:slug>/edit_comment/<int:comment_id>/',
+         comment_edit, name='comment_edit'),
+    path('post/<slug:slug>/delete_comment/<int:comment_id>/',
+         delete_comment, name='delete_comment'),
 ]
