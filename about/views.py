5ea62ba82f854ae54d6b8f89a0452e260ab3c49e
Werner <wstablein@hotmail.com>
2024-08-29 17:40:31 +0200

    Make reference to logic reviewed for about py class and explain rendering of object attributes in about_page.html

--- about/views.py
+++ about/views.py
@@ -3,6 +3,13 @@ from .models import AboutPage
 
 # Create your views here.
 
+## For this view, I used https://docs.djangoproject.com/en/dev/ref/models/querysets/#first
+## to understand that it is sufficient to simply retrieve the entire instance
+
+## The instance retrieved is passed on as context variable to the template
+
+
 def about_page(request):
     about_instance = AboutPage.objects.first()
     return render(request, 'about/about_page.html', {'about_instance': about_instance})
+
