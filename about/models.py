85f20bca37257eb3b67a43bced482bce274a3876
Werner <wstablein@hotmail.com>
2024-08-29 15:58:18 +0200

    Expand AboutPage model to ensure granularity of titles and styling

--- about/models.py
+++ about/models.py
@@ -1,10 +1,22 @@
 from django.db import models
 
+from django.db import models
+
 class AboutPage(models.Model):
     heading = models.CharField(max_length=255)
     about_image = models.ImageField(upload_to='about_images/')
     last_update = models.DateTimeField(auto_now=True)
-    about_text = models.TextField()
+    about_text = models.TextField()      
+    intro = models.TextField(blank=True)
+    my_story = models.TextField(blank=True)
+    why_created = models.TextField(blank=True)
+    comprehensive_analysis = models.TextField(blank=True)
+    personalized_insights = models.TextField(blank=True)
+    expert_opinions = models.TextField(blank=True)
+    conclusion = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.heading
 
     def __str__(self):
         return self.heading
