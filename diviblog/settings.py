8253dfd19ec2991d988078450065562d51d809ee
Werner <wstablein@hotmail.com>
2024-09-16 14:27:01 +0200

    Set debug false for final deyployment test

--- diviblog/settings.py
+++ diviblog/settings.py
@@ -19,7 +19,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: FOR PRODUCTION ON HEROKU DEBUG MUST BE FALSE
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'dividend-blog-app-7524309b6f0c.herokuapp.com',
